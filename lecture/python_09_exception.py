45f40f37c9d98c6fb688792423f02a47c3ee7778
tjehdwls <144027971+tjehdwls@users.noreply.github.com>
2023-10-27 14:32:08 +0900

    Add files via upload

---
+++ lecture/python_09_exception.py
@@ -0,0 +1,48 @@
+# 예외 처리
+# ** 예외(exception)
+#   - 프로그램을 개발 하면서 예상하지 못한상황
+#       ex) 사용자 입력 오류
+#   - 예외처리를 사용하면 프로그램이 종료 X
+#       입력 오류 -> 프로그램 종료
+#       입력 오류 -> 예외처리 -> 프로그램 종료 X
+#  - 데이터 베이스 또는 파일 시스템 사용할 때는 반드시 예외처리!!
+
+# ** 예외 종류
+# 1. 예측 가능한 예외
+#   - 발생 여부를 개발자가 사전에 인지 -> 예외 처리
+# 2. 예측 불가능한 예외
+#   - 서버에서 데이터를 받기(서버 화재발생, 장비고장으로 서버 종료)
+
+# ** 예외 기본 문법
+
+# try: 
+#    예외가 발생할수도 있는 코드
+# except 에외 타입:
+#   예외 발생시 실행된 코드(예외 처리)
+
+# else:
+#   예외가 발생하지 않는 경우 실행되는 코드
+# finallt:
+#   무조건 실행되는 코드(예외 발생여부 상관없음
+#   (대부분 자원 해제에 사용)
+
+# ** 자원해제: conneted를 끊기
+# 가정: 종합정보시스템(동시 접속 2000명)
+#   - 학생1 -> 종합정보시스템(Commented)
+#   - 학생2 -> 종합정보시스템(Commented)
+
+# 정상: try -> else -> finally
+# 예외: try -> except -> finally
+# try - except 한쌍
+# else finally 생략가능
+
+from urllib.request import urlopen, HTTPError
+
+try:
+    html = urlopen("http://www.navdjeinsoer.com")
+except HTTPError as e:
+    print(e)
+else:
+    print("No Error")
+finally:
+    print("자원해제")
