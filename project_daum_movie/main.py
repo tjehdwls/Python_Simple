7a74d32d31e5190dcaa5bfcf120833609b61c40c
tjehdwls <144027971+tjehdwls@users.noreply.github.com>
2023-12-01 14:26:06 +0900

    Add files via upload

--- project_daum_movie/main.py
+++ project_daum_movie/main.py
@@ -5,8 +5,8 @@
 #        5. DB에 저장된 데이터 -> WordCloud 시각화
 
 
-from collect.collect_daum_moive_review import review_collector
-
+from collect.collect_daum_movie_review import review_collector
+from db.movie_dao import get_last_review
 
 def main():
     print("="*100)
@@ -14,7 +14,27 @@ def main():
     print("="*100)
     movie_code = input("영화 코드>>") #169328
     print("알림: 매일12시간에 수집")
-    review_collector(movie_code)
+
+    # 중복리뷰 체크
+    # 1. 리뷰 수(X)
+    # - DB에 저장 된 리뷰 수(17)
+    # - 현재 수집 할 리뷰 수 (20)
+    # - 20-17=3 (리뷰 삭제 고려X)
+    # 2. 날짜 비교
+    # - Last_date = DB에 저장 된 랩 중 가장 최신 날짜 get
+    # - 수집리뷰_date 비교 Last_date
+    # 예시)
+    # Last_date = 2023.11.2. 02:25   ->   202311270225
+    # 수집리뷰_date = 2023.11.28. 02:25  -> 202311280225
+
+    lsat_date = get_last_review()
+
+    if lsat_date == None:
+        lsat_date = 0
+    else:
+        lsat_date = int(lsat_date["int_regdate"])
+
+    review_collector(movie_code, lsat_date)
 
 if __name__ == "__main__":
     main()
